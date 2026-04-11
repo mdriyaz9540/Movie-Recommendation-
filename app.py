e54782c81ac387150a57d6f93e455d567652436f
Mohd Riyaz <mdriyaz9540801@gmail.com>
2026-04-11 18:00:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle
+
+movies = pickle.load(open('movies.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+def recommend(movie):
+    if movie not in movies['title'].values:
+        return ["Movie not found"]
+
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+    return recommended_movies
+
+st.title("Movie Recommender 🎬")
+
+selected_movie = st.selectbox(
+    "Select a movie",
+    movies['title'].values
+)
+
+if st.button("Recommend"):
+    results = recommend(selected_movie)
+    for movie in results:
+        st.write(movie)
